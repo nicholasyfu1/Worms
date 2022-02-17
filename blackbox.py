658f8393c4a88f7e244c732faed2f3951469d6d7
Nicholas Fu <nicholasyfu1@gmail.com>
2022-02-17 16:17:32 -0500

    removed resolution from before capture and added resize parameter to capture

--- blackbox.py
+++ blackbox.py
@@ -190,8 +190,7 @@ class BehaviorBox(tk.Tk, Experiment):
 			self.frames[StimPrepPg].label2.configure(text="Time remaining: %d" % remaining) # Set countdown
 			self.frames[StimPrepPg].update_idletasks() # Refresh page            
 			if i%Appa.capturerate == 0: # Calculate if need to capture pic 
-				camera.resolution = (800, 480)
-				camera.capture(Appa.savefile + "/ExpDataPictures/image" + str(imgnum) + ".jpg")
+				camera.capture(Appa.savefile + "/ExpDataPictures/image" + str(imgnum) + ".jpg", resize=(800,480))
 				Appa.expy.append("") # Append empty place holder for future analyssi
 				imgnum+=1
 			sleep(1-(clock()-start_time))
