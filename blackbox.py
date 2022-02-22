354e5b48c3db4e1da581f01445536cb9bc1a59b1
Nicholas Fu <nicholasyfu1@gmail.com>
2022-02-22 15:01:07 -0500

    trying to fix flickering

--- blackbox.py
+++ blackbox.py
@@ -190,7 +190,7 @@ class BehaviorBox(tk.Tk, Experiment):
 			self.frames[StimPrepPg].label2.configure(text="Time remaining: %d" % remaining) # Set countdown
 			self.frames[StimPrepPg].update_idletasks() # Refresh page            
 			if i%Appa.capturerate == 0: # Calculate if need to capture pic
-				camera.resolution = (640,480) 
+				#camera.resolution = (640,480) 
 				camera.capture(Appa.savefile + "/ExpDataPictures/image" + str(imgnum) + ".jpg", resize=(640,480))
 				Appa.expy.append("") # Append empty place holder for future analyssi
 				imgnum+=1
