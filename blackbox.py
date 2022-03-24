04143f2447fec053dbddd1ec42e4fed93fc386f0
Nicholas Fu <nicholasyfu1@gmail.com>
2022-03-23 21:22:18 -0400

    last try 5fps

--- blackbox.py
+++ blackbox.py
@@ -191,7 +191,7 @@ class BehaviorBox(tk.Tk, Experiment):
 
 		#Image capturing
 		imgnum=0
-		fps=4
+		fps=5
 		numFrames=Appa.exptime*fps
 		seconds=0
 		for i in range(numFrames):
@@ -207,14 +207,16 @@ class BehaviorBox(tk.Tk, Experiment):
 			imgnum+=1
 
 			elapsed = clock() - startTime
-			if elapsed < 0.2:
-				sleep(0.2 - (elapsed))
+			if elapsed < 0.15:
+				sleep(0.15 - (elapsed))
 			
 			#clock() 1 fps (1.00 frameTime) is actually: ~1.13
 			#clock() 2 fps (0.50 frameTime) is actually: ~0.6
 			#clock() 3 fps (0.33 frameTime) is actually: ~0.42
 			#clock() 4 fps (0.25 frameTime) is actually: ~0.33
 			#clock() 4 fps (0.20 frameTime) is actually: ~0.33
+			#clock() 4 fps (0.20 frameTime) is actually: ~0.27 (removed resize(640,480) from camera.capture())
+			#clock() 5 fps (0.15 frameTime) is actually:       (removed resize)
 			
 			#time() 1.0 is actually: 0.9 to 1.1
 				
